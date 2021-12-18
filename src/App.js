98dc278db1b1cd0c0f495c920227b91d6f51128f
Evan Herman <evan.m.herman@gmail.com>
2021-12-18 18:32:12 -0500

    Fix spacing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import ReactDOM from 'react-dom'
 
 class App extends React.Component {
   constructor( props ) {
