41126596aa96e5302d6d74b174d0b2f0d035f1a8
Evan Herman <evan.m.herman@gmail.com>
2021-12-23 16:15:01 -0500

    Add World Totals component

---
+++ src/components/world-totals/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+class WorldTotals extends React.Component {
+  constructor( props ) {
+    super( props );
+  }
+
+  render() {
+    let confirmedTotal = this.props.covidData.reduce( ( n, { confirmed } ) => n + confirmed, 0 ).toLocaleString( "en-US" );
+    let recoveredTotal = this.props.covidData.reduce( ( n, { recovered } ) => n + recovered, 0 ).toLocaleString( "en-US" );
+    let criticalTotal = this.props.covidData.reduce( ( n, { critical } ) => n + critical, 0 ).toLocaleString( "en-US" );
+    let deathsTotal = this.props.covidData.reduce( ( n, { deaths } ) => n + deaths, 0 ).toLocaleString( "en-US" );
+    return (
+      <div className="grid grid-cols-4 gap-4">
+        <div className="text-center py-24">
+          <h2 className="text-4xl font-semibold">Confirmed</h2>
+          <img src="/images/confirmed.svg" className="py-4" />
+          <span className="text-2xl">{ confirmedTotal }</span>
+        </div>
+        <div className="text-center py-24">
+          <h2 className="text-4xl font-semibold">Recovered</h2>
+          <img src="/images/recovered.svg" className="py-4" />
+          <span className="text-2xl">{ recoveredTotal }</span>
+        </div>
+        <div className="text-center py-24">
+          <h2 className="text-4xl font-semibold">Critical</h2>
+          <img src="/images/critical.svg" className="py-4" />
+          <span className="text-2xl">{ criticalTotal }</span>
+        </div>
+        <div className="text-center py-24">
+          <h2 className="text-4xl font-semibold">Deaths</h2>
+          <img src="/images/deaths.svg" className="py-4" />
+          <span className="text-2xl">{ deathsTotal }</span>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default WorldTotals;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CountriesDataTable from './components/countries-data-table';
+import WorldTotals from './components/world-totals';
 
 import './App.css';
 
-fetch( "https://ipapi.co/json/", {
-  "method": "GET"
+// Get all COVID-19 data
+fetch( "https://www.covid19-api.com/country/all?format=json", {
+  "method": "GET",
+  "headers": {
+    "x-rapidapi-host": "covid-19-data.p.rapidapi.com"
+  }
 } )
-.then( ( res ) => res.json() )
-.then( response => {
-  ReactDOM.render(
-    <span>
-      <li className={`flag-icon flag-icon-${response.country_code?.toLowerCase()}`}></li>
-      {response.country_name}
-    </span>,
-    document.getElementById( 'user-country-flag' )
-  );
+.then( ( covidData ) => covidData.json() )
+.then( covidData => {
+  // Get geolocation data for user
+  fetch( "https://ipapi.co/json/", {
+    "method": "GET"
+  } )
+  .then( ( geoLocationData ) => geoLocationData.json() )
+  .then( geoLocationData => {
+    ReactDOM.render(
+      <span>
+        <span className={`flag-icon flag-icon-${geoLocationData.country_code?.toLowerCase()}`}></span>
+        {geoLocationData.country_name}
+      </span>,
+      document.getElementById( 'user-country-flag' )
+    );
 
-  ReactDOM.render(
-    <CountriesDataTable />,
-    document.getElementById( 'country-data-table' )
-  );
+    ReactDOM.render(
+      <WorldTotals covidData={covidData} />,
+      document.getElementById( 'world-totals' )
+    );
+
+    ReactDOM.render(
+      <CountriesDataTable />,
+      document.getElementById( 'country-data-table' )
+    );
+  } )
+  .catch( error => {
+    console.error( error );
+  } );
 } )
-.catch( error => {
-  console.error( error );
+.catch( err => {
+  console.error( err );
 } );
