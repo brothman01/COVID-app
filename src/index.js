31a2d299252b3f4d5613cc027218471cb5b59081
Evan Herman <evan.m.herman@gmail.com>
2021-12-23 12:38:19 -0500

    Remove import App

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import CountriesDataTable from './components/countries-data-table';
 
 // ReactDOM.render(
