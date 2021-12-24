9ffc8eb13100da7079818edcc8ecbd0e9d0f7dca
Evan Herman <evan.m.herman@gmail.com>
2021-12-24 18:27:40 -0500

    Add Google bar chart to landing page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Internal Componenets
 import CountriesDataTable from './components/countries-data-table';
 import WorldTotals from './components/world-totals';
 
+// External Componenets
+import Chart from "react-google-charts";
+
+// Styles
 import './App.css';
+
+function getData( covidData ) {
+  covidData.sort( function( a, b ) {
+    var keyA = new Date( a.confirmed ),
+        keyB = new Date( b.confirmed );
+    if (keyA < keyB) return -1;
+    if (keyA > keyB) return 1;
+    return 0;
+  } );
+  let topFiveCountries = covidData.slice( -5 ).reverse();
+  let tableData = [
+    [ 'City', 'Confirmed', 'Recovered', 'Deaths' ],
+  ];
+  topFiveCountries.forEach( ( country ) => {
+    tableData.push(  [
+      country.country,
+      country.confirmed,
+      country.recovered,
+      country.deaths,
+    ] );
+  } );
+  console.log( tableData );
+  return tableData;
+}
 
 // Get all COVID-19 data
 fetch( "https://www.covid19-api.com/country/all?format=json", {
@@ -37,6 +67,27 @@ fetch( "https://www.covid19-api.com/country/all?format=json", {
       <CountriesDataTable />,
       document.getElementById( 'country-data-table' )
     );
+
+    ReactDOM.render(
+      <Chart
+        chartType="ColumnChart"
+        height={800}
+        loader={<div>Loading Chart</div>}
+        data={getData( covidData )}
+        options={{
+          title: 'Top 5 Countries with Most COVID-19 Cases',
+          chartArea: { width: '100%' },
+          hAxis: {
+            title: 'Country',
+            minValue: 0,
+          },
+          vAxis: {
+            title: 'Total Cases',
+          },
+        }}
+      />,
+      document.getElementById( 'googleColumnChart' )
+    )
   } )
   .catch( error => {
     console.error( error );
