88beca149eb3773d3ff9d763484e58d6ffc5e78e
Evan Herman <evan.m.herman@gmail.com>
2021-12-24 10:47:44 -0500

    Fix world totals responsiveness

--- src/components/world-totals/index.js
+++ src/components/world-totals/index.js
@@ -8,23 +8,23 @@ class WorldTotals extends React.Component {
     let deathsTotal = this.props.covidData.reduce( ( n, { deaths } ) => n + deaths, 0 ).toLocaleString( "en-US" );
     let deathPercent = parseFloat((this.props.covidData.reduce( ( n, { deaths } ) => n + deaths, 0 )/this.props.covidData.reduce( ( n, { confirmed } ) => n + confirmed, 0 ))*100).toFixed(2)+"%";
     return (
-      <div className="grid grid-cols-4 gap-4">
-        <div className="text-center py-24">
+      <div className="grid grid-cols-1 md:grid-cols-4">
+        <div className="text-center py-10 md:py-24">
           <h2 className="text-4xl font-semibold">Confirmed</h2>
           <img src="images/confirmed.svg" alt="Confirmed Icon" className="py-4" />
           <span className="text-2xl">{ confirmedTotal }</span>
         </div>
-        <div className="text-center py-24">
+        <div className="text-center py-10 md:py-24">
           <h2 className="text-4xl font-semibold">Recovered</h2>
           <img src="images/recovered.svg" alt="Recovered Icon" className="py-4" />
           <span className="text-2xl">{ recoveredTotal }</span>
         </div>
-        <div className="text-center py-24">
+        <div className="text-center py-10 md:py-24">
           <h2 className="text-4xl font-semibold">Critical</h2>
           <img src="images/critical.svg" alt="Critical Icon" className="py-4" />
           <span className="text-2xl">{ criticalTotal }</span>
         </div>
-        <div className="text-center py-24">
+        <div className="text-center py-10 md:py-24">
           <h2 className="text-4xl font-semibold">Deaths</h2>
           <img src="images/deaths.svg" alt="Deaths Icon" className="py-4" />
           <span className="text-2xl">{ deathsTotal }</span><br />
