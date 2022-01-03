7d2b7ed16fd2122a1ea25ff41acc839d1170b59f
Evan Herman <evan.m.herman@gmail.com>
2022-01-03 11:40:15 -0500

    Ability to reset selected country back to world stats

--- src/components/live-data-heading/index.js
+++ src/components/live-data-heading/index.js
@@ -3,10 +3,15 @@ import React from 'react';
 class LiveDataHeading extends React.Component {
   render() {
     return (
-      <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800" style={{position: 'relative'}}>
-        <div id="live-data-header" style={{position: 'absolute', top: -100 + 'px', left: 0}}></div>
-        Live Data <br />({this.props.selectedCountry.country || "World Stats"})
-      </h1>
+      <>
+        { !! this.props.selectedCountry && (
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded gradient show-world-data" onClick={() => this.props.updateSelectedCountry(false)}>Show World Stats</button>
+        )}
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-gray-800" style={{position: 'relative'}}>
+          <div id="live-data-header" style={{position: 'absolute', top: -175 + 'px', left: 0}}></div>
+          Live Data <br />({this.props.selectedCountry.country || "World Stats"})
+        </h1>
+      </>
     );
   }
 }
